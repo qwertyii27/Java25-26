c8ab92a2e3f427597350235c29b9902ed17a2f08
Georgio Tournas <27gtournas@derryfield.org>
2025-10-08 15:11:42 -0400

    Tested Arrays

---
+++ RandomPrograms/Array_Test.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class Array_Test {
+    public static void main(String[] args) {
+        String[] cars = new String[4];
+        cars[1] = "BMW";
+        cars[2] = "Honda";
+        cars[3] = "Toyota";
+        System.out.println(Arrays.toString(cars));
+    }
+
+}
