9ca02decbbe8ffe2d8c381a345b8a109d5ce3a49
Georgio Tournas <27gtournas@derryfield.org>
2025-10-15 13:45:05 -0400

    Create Temperature_PartA.java

---
+++ Java06Projects/Temperature_PartA.java
@@ -0,0 +1,56 @@
+
+// Made by Georgio
+import java.util.Scanner;
+
+public class Temperature_PartA {
+    public static void main(String[] args) {
+        // declare vars
+        Scanner in = new Scanner(System.in);
+        String type;
+        double heat;
+        boolean gas = false;
+        boolean liquid = false;
+        boolean solid = false;
+
+        // get input
+        System.out.println("Enter a number and string.");
+        heat = in.nextDouble();
+        type = in.next();
+
+        System.out.printf("%f%s%n", heat, type);
+        System.out.println("_________"); // seperator
+
+        // calculations
+        if (type.startsWith("C")) {
+            if (heat >= 100.0) {
+                gas = true;
+            } else if (heat >= 0) {
+                liquid = true;
+            } else {
+                solid = true;
+            }
+        } else {
+            if (heat >= 212.0) {
+                gas = true;
+            } else if (heat >= 32.0) {
+                liquid = true;
+            } else {
+                solid = true;
+            }
+
+        }
+
+        // give output
+        if (gas = true) {
+            System.out.printf("At a temperature of $.2f%s, water is a gas.", heat, type);
+        }
+        if (liquid = true) {
+            System.out.printf("At a temperature of $.2f%s, water is a liquid.", heat, type);
+        }
+        if (solid = true) {
+            System.out.printf("At a temperature of $.2f%s, water is a solid.", heat, type);
+        }
+
+        in.close();
+    }
+}
