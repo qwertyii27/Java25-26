952566ed4b984d7744b07d88c6d864c3f2c783b1
qwertyii27 <27gtournas@derryfield.org>
2025-10-27 13:54:18 -0400

    Create LoopTest.java

---
+++ JavaCW/LoopTest.java
@@ -0,0 +1,9 @@
+public class LoopTest {
+    public static void main(String[] args) {
+        int i;
+        for (i = 1; i < 6; i++) {
+            System.out.println(i);
+        }
+        System.out.println("i is " + i);
+    }
+}
