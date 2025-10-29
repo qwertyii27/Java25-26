d73fa04c9266d060692b07f01826855104de6ea2
qwertyii27 <27gtournas@derryfield.org>
2025-10-29 18:17:06 -0400

    Finished ticket project

--- JavaProject03/TicketProject1.java
+++ JavaProject03/TicketProject1.java
@@ -13,6 +13,10 @@ public class TicketProject1 {
         MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY
     }
 
+    enum Price {
+        REGULAR, RUSH, WEEKEND
+    }
+
     public static void main(String[] args) {
         System.out.printf("Launching ticketing system...%n-----------------------%n ");
         // enmurate age and days
@@ -20,12 +24,14 @@ public class TicketProject1 {
         // initialize variables
         int hoursIn, minutesIn, timeMil, ageIn;
         String timeIn, timeWork, name, dayIn, studentIn;
+        String studentMessage;
         final double PRICE_WEEKEND = 80.00, PRICE_RUSH = 100.00, PRICE_NORMAL = 90.00;
         final double BABY_DISCOUNT = 0.0, CHILD_DISCOUNT = 0.8, SENIOR_DISCOUNT = 0.75;
         final double STUDENT_DISCOUNT = 0.95;
-        double finalPrice;
+        double basePrice = 0, finalPrice = 0, ageDiscount = 0, studentDiscount = 0;
         boolean student;
         Day today;
+        Price price;
         Age age = null;
 
         // initialize scanner
@@ -36,16 +42,19 @@ public class TicketProject1 {
         name = in.nextLine();
         if (name.isEmpty()) { // check for name actually being there
             System.err.printf("%nError: You have not entered a name.%nTerminating program.%n");
+            in.close();
             return;
         }
 
         // get day of week
-        System.out.printf("%nPlease enter the day of the week: ");
+        System.out.printf("Please enter the day of the week: ");
         dayIn = in.next().toLowerCase();
         if (dayIn.isEmpty()) { // check for day actually being there
             System.err.printf("%nError: You have not entered a day.%nTerminating program.%n");
+            in.close();
             return;
         }
+        // enum day of week
         switch (dayIn) {
             case "mon":
             case "monday":
@@ -78,44 +87,55 @@ public class TicketProject1 {
                 today = Day.SUNDAY;
                 break;
             default:
-                System.err.printf("%nError: Day input is inccorect. Please enter correctly.%nTerminating.%n");
+                System.err.printf("Error: Day input is inccorect. Please enter correctly.%nTerminating.%n");
+                in.close();
                 return;
 
         }
 
         // get time
-        System.out.printf("%nPlease enter the 24hr time (HH:MM): ");
+        System.out.printf("Please enter the 24hr time (HH:MM): ");
         timeIn = in.next();
-        if (!timeIn.contains(":")) {
-            System.err.printf("Error: Please input the time in this format: \"HH:MM\"");
+        if (timeIn.charAt(2) != ':' || !Character.isDigit(timeIn.charAt(0)) || !Character.isDigit(timeIn.charAt(1))
+                || !Character.isDigit(timeIn.charAt(3)) || !Character.isDigit(timeIn.charAt(4))) {
+            System.err.printf("Error: Please input the time in this format: \"HH:MM\"%nTerminating program.");
+            in.close();
             return;
         }
+        // parse time
         hoursIn = Integer.parseInt(timeIn.substring(0, 2));
         minutesIn = Integer.parseInt(timeIn.substring(3, 5));
         timeMil = Integer.parseInt(String.valueOf(hoursIn) + String.valueOf(minutesIn));
         timeWork = String.valueOf(hoursIn) + ":" + String.valueOf(minutesIn);
-        if (hoursIn < 0 || hoursIn > 23 || minutesIn > 59 || minutesIn < 0) {
+        if (hoursIn < 0 || hoursIn > 23 || minutesIn > 59 || minutesIn < 0) { // check for format
             System.err.printf("Error: Please input a proper time in the 24 hour format.%nTerminating Program.%n");
+            in.close();
             return;
         }
         // get age
-        System.out.printf("%nPlease input the passenger's age:");
+        System.out.printf("Please input the passenger's age:");
         ageIn = in.nextInt();
-        if (ageIn < 0) {
+        if (ageIn < 0) { // silly
             System.err.printf("%nError: Unborn children cannot buy train tickets.%n");
         }
+        // enum age range
         if (ageIn >= 60) {
             age = Age.SENIOR;
+            ageDiscount = SENIOR_DISCOUNT;
         } else if (ageIn >= 13 && ageIn <= 59) {
             age = Age.ADULT;
+            ageDiscount = 1;
         } else if (ageIn >= 3 && ageIn <= 12) {
             age = Age.CHILD;
+            ageDiscount = CHILD_DISCOUNT;
         } else if (ageIn >= 0 && ageIn <= 2) {
             age = Age.BABY;
+            ageDiscount = BABY_DISCOUNT;
         }
-        System.out.printf("%nAre you a student? (y/n): ");
+        // check for student
+        System.out.printf("Are you a student? (y/n): ");
         studentIn = in.next();
-        if (!studentIn.equals("n") && !studentIn.equals("y")) {
+        if (!studentIn.equals("n") && !studentIn.equals("y")) { // just be normal..
             System.err.printf("%nError: Please enter answer as y/n.%nTerminating program.");
             in.close();
             return;
@@ -126,22 +146,32 @@ public class TicketProject1 {
             student = true;
         }
 
-        // find base pricing
+        // enumerate base pricing
         if (today == Day.MONDAY || today == Day.TUESDAY || today == Day.WEDNESDAY || today == Day.THURSDAY
                 || today == Day.FRIDAY) {
             if (timeMil >= 600 && timeMil <= 759) {
-                finalPrice = PRICE_RUSH;
-                boolean rushHour = true;
+                price = Price.RUSH;
             } else if (timeMil >= 1600 && timeMil <= 1759) {
-                finalPrice = PRICE_RUSH;
+                price = Price.RUSH;
             } else {
-                finalPrice = PRICE_NORMAL;
+                price = Price.REGULAR;
             }
-
         } else {
-            finalPrice = PRICE_WEEKEND;
-            boolean rushHour = false;
+            price = Price.WEEKEND;
         }
+        // calculate base pricing
+        switch (price) {
+            case RUSH:
+                basePrice = PRICE_RUSH;
+                break;
+            case REGULAR:
+                basePrice = PRICE_NORMAL;
+                break;
+            case WEEKEND:
+                basePrice = PRICE_WEEKEND;
+                break;
+        }
+        finalPrice = basePrice;
 
         // find age discounts
         switch (age) {
@@ -160,13 +190,26 @@ public class TicketProject1 {
         // find student discount
         if (student = true) {
             finalPrice = finalPrice * STUDENT_DISCOUNT;
+            studentMessage = "ACTIVE";
+            studentDiscount = STUDENT_DISCOUNT;
+        } else {
+            studentMessage = "INACTIVE";
+            studentDiscount = 0;
         }
-        System.out.println(name);
-        System.out.println(finalPrice);
-        System.out.println(timeWork);
-        System.out.println(age);
-        System.out.println("Student?" + student);
-        System.out.println(today);
+        // final output
+        System.out.printf(
+                "%n---------------------------------%nName:    %-20s%nDay:     %-20s%nTime:    %-20s%n---------------------------------%n",
+                name,
+                today, timeWork);
+        System.out.printf("Base Price:         $%11.2f (%s)%n", basePrice, price);
+        System.out.printf("Age Discount:       $%11.2f (%s)%n", (basePrice * ageDiscount) - basePrice, age);
+        System.out.printf("Student Discount:   $%11.2f (%s)",
+                (basePrice * (ageDiscount) * studentDiscount) - (basePrice * ageDiscount),
+                studentMessage);
+        System.out.printf("%n---------------------------------%n");
+        System.out.printf("Final price:        $%11.2f", finalPrice);
+        System.out.printf("%n---------------------------------%n");
+
         in.close();
     }
 }
