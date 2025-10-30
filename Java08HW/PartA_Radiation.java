86e9c55920b626a5adcec86c27ff3f715b86e459
qwertyii27 <27gtournas@derryfield.org>
2025-10-30 13:32:19 -0400

    Make PartA-B templates

---
+++ Java08HW/PartA_Radiation.java
@@ -0,0 +1,5 @@
+public class PartA_Radiation {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Java08HW/PartB_APR.java
@@ -0,0 +1,5 @@
+public class PartB_APR {
+    public static void main(String[] args) {
+
+    }
+}
