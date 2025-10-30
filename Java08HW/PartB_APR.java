bc8a7c9cb7d781460354b3b8ef26d7f381a68088
qwertyii27 <27gtournas@derryfield.org>
2025-10-30 14:55:11 -0400

    Update PartB_APR.java

--- Java08HW/PartB_APR.java
+++ Java08HW/PartB_APR.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 public class PartB_APR {
     public static void main(String[] args) {
-
+        int year;
+        double apr, interest, total;
+        Scanner in = new Scanner(System.in);
     }
 }
