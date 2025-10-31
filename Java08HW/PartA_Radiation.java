b8ec7fe23fba98ff3dd9b696360490fc63dc343c
qwertyii27 <27gtournas@derryfield.org>
2025-10-31 08:37:25 -0400

    Finished HW

--- Java08HW/PartA_Radiation.java
+++ Java08HW/PartA_Radiation.java
@@ -1,5 +1,15 @@
+//By georgio
+//I searched up decay functions to test this one.
 public class PartA_Radiation {
     public static void main(String[] args) {
+        double remaining = 100;
+        int i = 0;
+        while (remaining > 1) {
+            remaining = remaining * 1 / 2;
+            i++;
+        }
+        System.out.printf("Remaining material: %.2f percent%n", remaining);
+        System.out.printf("Years taken to reach this point: %d%n", i * 30);
 
     }
 }
--- Java08HW/PartB_APR.java
+++ Java08HW/PartB_APR.java
@@ -2,8 +2,20 @@ import java.util.Scanner;
 
 public class PartB_APR {
     public static void main(String[] args) {
-        int year;
-        double apr, interest, total;
+        double apr, initial, total, earned;
         Scanner in = new Scanner(System.in);
+
+        System.out.println("APR (enter as double): ");
+        apr = in.nextDouble() / 100;
+        System.out.println("Principal amount (enter as double): ");
+        initial = in.nextDouble();
+        System.out.println("Year |   Interest ($).       |        Balance ($)");
+        System.out.println("-------------------------------------------------");
+
+        for (int i = 1; i <= 5; i++) {
+            earned = -(initial - (initial + (initial * apr)));
+            initial = initial + (initial * apr);
+            System.out.printf(" %d  |%10.2f|%10.2f%n", i, earned, initial);
+        }
     }
 }
