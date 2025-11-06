72bd7ec11b3a9046f432a0742b443f74889e0b8d
qwertyii27 <27gtournas@derryfield.org>
2025-11-06 11:25:43 -0500

    Added Method Test

---
+++ RandomPrograms/Method_Test.java
@@ -0,0 +1,10 @@
+public class Method_Test {
+    public static double quotient(double x, double y) {
+        return x / y;
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(quotient(6, 3));
+    }
+}
