9c16a0e693add8da5962a02ffcf5fd69fd7752b5
qwertyii27 <27gtournas@derryfield.org>
2025-12-09 10:50:49 -0500

    Create Method_Average.java

---
+++ JavaCW/Method_Average.java
@@ -0,0 +1,12 @@
+public class Method_Average {
+    public static double doubleAvg(double num1, double num2) {
+        double numSum = num1 + num2;
+
+        return numSum / 2.0;
+
+    }
+
+    public static void main(String[] args) {
+        System.out.print(doubleAvg(10, 3));
+    }
+}
