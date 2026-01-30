4823cc187e4ac9862bee6ac43416aadc69fb7d6e
qwertyii27 <27gtournas@derryfield.org>
2026-01-30 10:43:02 -0500

    Added delimiter example

---
+++ JavaCW/DelimiterExample.java
@@ -0,0 +1,18 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class DelimiterExample {
+    public static void main(String[] args) throws FileNotFoundException {
+        File inputFile = new File("poem.txt");
+        Scanner in = new Scanner(inputFile);
+        in.useDelimiter("[^A-Za-z0-9]+");
+        int count = 0;
+        while (in.hasNext()) {
+            String word = in.next();
+            System.out.println(word);
+            count++;
+        }
+        System.out.printf("%d words were scanned in.", count);
+    }
+}
