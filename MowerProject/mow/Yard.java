3bebdc451fd709f336a67051689270ee2c040771
qwertyii27 <27gtournas@derryfield.org>
2026-03-12 10:41:03 -0400

    more work on Yard for project

--- MowerProject/mow/Yard.java
+++ MowerProject/mow/Yard.java
@@ -7,6 +7,14 @@ class Yard {
   int height;
   int width;
 
+  public int getHeight() {
+    return height;
+  }
+
+  public int getWidth() {
+    return width;
+  }
+
   public Yard(int height, int width) {
     // add buffer for fence
     this.height = height + 2;
@@ -14,6 +22,18 @@ class Yard {
 
     // initialize array
     char[][] yardArr = new char[this.height][this.width];
+
+  
+    public char findSpace(int height, int width) {
+    return yardArr[height][width];
+  }
+
+    // establish lawn
+    for (int i = 0; i < this.height; i++) {
+      for (int r = 0; r < this.width; r++) {
+        yardArr[i][r] = '+';
+      }
+    }
 
     // establish brick fence
     for (int i = 0; i < this.width; i++) {
