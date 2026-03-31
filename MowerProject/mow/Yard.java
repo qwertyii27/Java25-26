0250e0541bed64557463a18b4b66cb081dd709eb
qwertyii27 <27gtournas@derryfield.org>
2026-03-31 12:09:16 -0400

    Finished PartA.

--- MowerProject/mow/Yard.java
+++ MowerProject/mow/Yard.java
@@ -1,11 +1,10 @@
-package mow;
-
 // + = unmowed
 // R = wall
 // empty is mowed
 class Yard {
   int height;
   int width;
+  char[][] yardArr;
 
   public int getHeight() {
     return height;
@@ -15,18 +14,30 @@ class Yard {
     return width;
   }
 
+  public char getSpace(int height, int width) {
+    return yardArr[height][width];
+  }
+
+  public void setSpace(int height, int width, char value) {
+    yardArr[height][width] = value;
+  }
+
+  // prints out the lawn.
+  public void printLawn() {
+    for (int i = 0; i < this.width; i++) {
+      for (int r = 0; r < this.height; r++)
+        System.out.print(yardArr[r][i]);
+      System.out.println();
+    }
+  }
+
   public Yard(int height, int width) {
     // add buffer for fence
     this.height = height + 2;
     this.width = width + 2;
 
     // initialize array
-    char[][] yardArr = new char[this.height][this.width];
-
-  
-    public char findSpace(int height, int width) {
-    return yardArr[height][width];
-  }
+    yardArr = new char[this.height][this.width];
 
     // establish lawn
     for (int i = 0; i < this.height; i++) {
@@ -38,13 +49,18 @@ class Yard {
     // establish brick fence
     for (int i = 0; i < this.width; i++) {
       yardArr[0][i] = 'R';
-      yardArr[this.height][i] = 'R';
+      yardArr[this.height - 1][i] = 'R';
     }
 
-    for (int i = 0; i < this.height; i++) {
+    for (int i = 0; i < this.height - 1; i++) {
       yardArr[i][0] = 'R';
-      yardArr[i][this.width] = 'R';
+      yardArr[i][this.width - 1] = 'R';
     }
 
+  }
+
+  public static void main(String[] args) {
+    Yard yard = new Yard(10, 10);
+    yard.printLawn();
   }
 }
