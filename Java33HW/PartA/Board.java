5e2765ada809d7216822285512fdd4fc61315c12
qwertyii27 <27gtournas@derryfield.org>
2026-04-16 23:31:18 -0400

    finished animation HW 02

--- Java33HW/PartA/Board.java
+++ Java33HW/PartA/Board.java
@@ -5,39 +5,86 @@ import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.geom.AffineTransform;
+import java.awt.geom.Ellipse2D;
 import java.awt.Shape;
 import java.awt.Rectangle;
 import java.io.File;
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
+import java.awt.geom.Ellipse2D;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class Board extends JPanel {
-  private final int B_WIDTH = 350;
-  private final int B_HEIGHT = 350;
+  private final int B_WIDTH = 720;
+  private final int B_HEIGHT = 720;
   private BufferedImage img;
+  private int x = 0;
+  private int y = 0;
+  private double rotate = 0;
+  private final int DIAMETER = 20;
+  private Timer timer;
+  private final int INITIAL_DELAY = 100;
+  private final int PERIOD_INTERVAL = 25;
+  private int xSpeed = 1;
+  private int ySpeed = 1;
+  private double rotateSpeed = Math.PI / 90.0;
+
+  private class ScheduledUpdate extends TimerTask {
+    /*
+     * Override the run() method.
+     * Update the position of our ball here.
+     */
+    public void run() {
+      x += xSpeed;
+      if (x > B_WIDTH) {
+        x = 0;
+      }
+      y += ySpeed;
+      if (y > B_HEIGHT) {
+        y = 0;
+      }
+      rotate += rotateSpeed;
+      repaint();
+    }
+  }
 
   public Board() {
+    // set background color of the board and default size.
     setBackground(Color.CYAN);
-    // attempt to load the image.
+    setPreferredSize(new Dimension(B_WIDTH, B_HEIGHT));
+
+    // set the initial position of the ball
+    // to be on the left side of the content area
+    // and in the middle of the content area.
+    x = 0;
+    y = 0;
+    rotate = 0;
+
     try {
-      File imageFile = new File("media/cakes.jpg");
+      File imageFile = new File("media/Andy.png");
       img = ImageIO.read(imageFile);
-      setPreferredSize(new Dimension(img.getWidth(), img.getHeight()));
+      setPreferredSize(new Dimension(B_WIDTH, B_HEIGHT));
     } catch (Exception e) {
       System.err.println(e.getMessage());
     }
+
+    timer = new Timer();
+    timer.scheduleAtFixedRate(new ScheduledUpdate(),
+        INITIAL_DELAY, PERIOD_INTERVAL);
+
   }
 
   public void paintComponent(Graphics g) {
     super.paintComponent(g);
 
+    AffineTransform affineTransform = new AffineTransform();
+    affineTransform.translate(x, y);
+    affineTransform.rotate(rotate, img.getWidth() / 2.0, img.getHeight() / 2.0);
+
     Graphics2D g2d = (Graphics2D) g;
     if (img != null) {
-      g2d.drawImage(img, null, null);
+      g2d.drawImage(img, affineTransform, null);
     }
-    // set background color of the board and default size.
-    setBackground(Color.CYAN);
-    setPreferredSize(new Dimension(B_WIDTH, B_HEIGHT));
-
   }
 }
--- Java33HW/PartB/Board.java
+++ Java33HW/PartB/Board.java
@@ -1,73 +1,51 @@
 
+//NOTE: Since the image I chose was so small, I elected to make the content area larger rather than transforming the image.
 import javax.swing.JPanel;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.geom.AffineTransform;
-import java.awt.geom.Ellipse2D;
 import java.awt.Shape;
 import java.awt.Rectangle;
 import java.io.File;
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
-import java.awt.geom.Ellipse2D;
-import java.util.Timer;
-import java.util.TimerTask;
 
 public class Board extends JPanel {
-  private final int B_WIDTH = 350;
-  private final int B_HEIGHT = 350;
   private BufferedImage img;
-  private int x = 0;
-  private int y = 0;
-  private final int DIAMETER = 20;
-  private Timer timer;
-  private final int INITIAL_DELAY = 100;
-  private final int PERIOD_INTERVAL = 25;
-  private int xSpeed = 2;
-
-  private class ScheduledUpdate extends TimerTask {
-    /*
-     * Override the run() method.
-     * Update the position of our ball here.
-     */
-    public void run() {
-      x += xSpeed;
-      if (x > B_WIDTH) {
-        x = 0;
-      }
-      repaint();
-    }
-  }
+  // private final int B_WIDTH;
+  // private final int B_HEIGHT;
 
   public Board() {
-    // set background color of the board and default size.
     setBackground(Color.CYAN);
-    setPreferredSize(new Dimension(B_WIDTH, B_HEIGHT));
-
-    // set the initial position of the ball
-    // to be on the left side of the content area
-    // and in the middle of the content area.
-    x = 0;
-    y = B_HEIGHT / 2;
-
-    timer = new Timer();
-    timer.scheduleAtFixedRate(new ScheduledUpdate(),
-        INITIAL_DELAY, PERIOD_INTERVAL);
-
+    // attempt to load the image.
+    try {
+      File imageFile = new File("media/shrimpfriedrice.png");
+      img = ImageIO.read(imageFile);
+      final int B_WIDTH = img.getWidth();
+      final int B_HEIGHT = img.getHeight();
+      setPreferredSize(new Dimension(B_WIDTH, B_HEIGHT));
+    } catch (Exception e) {
+      System.err.println(e.getMessage());
+    }
   }
 
   public void paintComponent(Graphics g) {
     super.paintComponent(g);
 
     AffineTransform affineTransform = new AffineTransform();
-    affineTransform.translate(x - DIAMETER / 2, y - DIAMETER / 2);
+    affineTransform.translate(img.getWidth() - (img.getWidth() / 4), img.getHeight() - (img.getHeight() / 4));
+    affineTransform.scale(0.25, 0.25);
 
     Graphics2D g2d = (Graphics2D) g;
-    Ellipse2D ellipse = new Ellipse2D.Double(0, 0, DIAMETER, DIAMETER);
-    Shape transformedShape = affineTransform.createTransformedShape(ellipse);
-    g2d.setColor(Color.MAGENTA);
-    g2d.fill(transformedShape);
+    if (img != null) {
+      g2d.drawImage(img, affineTransform, null);
+    }
+    // set background color of the board and default size.
+    setBackground(Color.CYAN);
+    // setPreferredSize(new Dimension(img.getWidth() * 4, img.getHeight() * 4));
+    // setPreferredSize(new Dimension(B_WIDTH, B_HEIGHT));
+
   }
 }
